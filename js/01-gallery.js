c656f9b18ac772c5082c5fc38846dde9a44dd31d
Prime-DS <prime.up17@gmail.com>
2022-07-11 00:42:42 +0300

    add task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery')
+const galleryMarkup = createGalaryHtml(galleryItems);
+galleryEl.insertAdjacentHTML("beforeend", galleryMarkup)
+
+function createGalaryHtml(galleryItems) {
+        return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+  <a class="gallery__link" target="_self" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+    }).join("");
+}
+
+galleryEl.addEventListener("click", onModal);
+
+function onModal(eve) {
+  eve.preventDefault()
+  if (!eve.target.classList.contains('gallery__image')
+  ) {
+    return;
+  }
+const instance = basicLightbox.create(`<img src="${eve.target.dataset.source}">`);
+  instance.show();
+}
+
+
+
+
